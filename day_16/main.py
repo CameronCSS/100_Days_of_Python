c25421a942f240627c5efc479e089047c0182988
Cameron Seamons <CameronSeamons@gmail.com>
2024-10-18 21:28:18 -0600

    Finished coding coffe_machine

--- day_16/main.py
+++ day_16/main.py
@@ -4,16 +4,33 @@ from money_machine import MoneyMachine
 
 
 def main() -> None:
-    pass
+    coffee_maker = CoffeeMaker()
+    menu = Menu()
+    money_machine = MoneyMachine()
+    
 
+    while True:
+        choice = input(f"What would you like? ({menu.get_items()})")
+        if choice == 'report':
+            coffee_maker.report()
+            money_machine.report()
+            continue
+        elif choice == 'off':
+            return
+        
+        drink = menu.find_drink(choice)
+        if drink:
+            pass
+        else:
+            continue
 
-def upper_everything(elements: list[str]) -> list[str]:
-    return [element.upper() for element in elements]
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+        else:
+            continue
 
-
-list_1: list[str] = upper_everything(['mario', 'james', 'sandra'])
-
-print(list_1)
 
 if __name__ == '__main__':
     main()
+
