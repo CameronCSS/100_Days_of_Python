074554b884a5ea5c7273f046c54bb6285870e8c2
Cameron Seamons <CameronSeamons@gmail.com>
2024-11-13 23:53:54 -0700

    finished day 26

---
+++ day_26/doubled.py
@@ -0,0 +1,11 @@
+def main(x,y) -> None:
+    doubled = [num * 2 for num in range(x,y)]
+
+
+    print(doubled)
+    
+
+
+if __name__ == '__main__':
+    main(1,5)
+    
---
+++ nato_phonetic_letters/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+
+pwd = os.getcwd()
+
+
+def main() -> None:
+
+    data=pd.read_csv(f"{pwd}\\nato_phonetic_alphabet.csv")
+    
+    nato_dict = {row['letter']: row['code'] for index, row in data.iterrows()}
+    
+    user_word = input("Enter a word: ").upper()
+
+    output = [nato_dict[letter] if letter in nato_dict else 'Letter Not found' for letter in user_word]
+    
+    print(output)
+
+if __name__ == '__main__':
+    main()
+    
+    
